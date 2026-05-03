ace934fd45bb815c4a7d66dc7b15956c2c2b0cbe
archanaprabhat <archanaprabhath2002@gmail.com>
2026-05-03 15:08:56 +0530

    add script for animated percentage counter

--- sciprt.js
+++ sciprt.js
@@ -1,16 +1,23 @@
-const el = document.getElementById("count")
+const el = document.getElementById("count");
 
 const target = 92;
-const duration =  900;
+const duration = 900;
 
 let startTime = null;
 
-
-function animate(time){
-    if(!startTime) startTime = time
-    const elapsed = time - startTime;
-    const progress = Math.min(elapsed/duration,1)
+function animate(time) {
+  if (!startTime) startTime = time;
+  const elapsed = time - startTime;
+  const progress = Math.min(elapsed / duration, 1);
+  const easeOut = 1 - Math.pow(1 - progress, 3);
+  const current = Math.floor(easeOut * target);
+  el.textContent = current;
+  if (progress < 1) {
+    requestAnimationFrame(animate);
+  } else {
+    el.textContent = target;
+    document.querySelector(".arrow").classList.add("animate-arrow");
+  }
 }
 
-
-requestAnimationFrame(animate)
+requestAnimationFrame(animate);
